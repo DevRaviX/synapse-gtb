bf38e24c854f076126345ff99b7052a6f16e5f1a
anjeetK <anjeetkeshari141002@gmail.com>
2026-03-20 03:39:00 +0530

    model trained with 1.5M rows

--- check_csv.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_csv('c:/Users/anjee/Desktop/Project-chronos/datasets_13339.csv', nrows=5)
-print(df.columns.tolist())
--- check_csv_cols.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_csv('c:/Users/anjee/Desktop/Project-chronos/datasets_13339.csv', nrows=1)
-print(",".join(df.columns.tolist()))
